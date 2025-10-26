ca9ad176ad423d61cb6f064fb4fb15af57cd3979
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2025-10-26 09:42:20 +0900

    챕터/레벨데이터 json 파싱기능 구현

---
+++ Scripts/car.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name Car
+
+var currentRoadIdx: int
+
+func _init(initialRoadIdx: int) -> void:
+    currentRoadIdx = initialRoadIdx
+
+func _ready() -> void:
+    pass # Replace with function body.
+
+func _process(delta: float) -> void:
+    pass
